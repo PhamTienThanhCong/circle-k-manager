751f616c9213385a46d83a3087a1bc93be590405
congphamtienthanh <congphamtienthanh@gmail.com>
2022-06-30 23:30:42 +0700

    first commit

---
+++ database/seeds/DatabaseSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+use Database\Seeders\DepartmentSeeder;
+use Database\Seeders\RoleSeeder;
+use Database\Seeders\UserSeeder;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        try {
+            
+            // $this->call(DepartmentSeeder::class);
+            // $this->call(RoleSeeder::class);
+            // $this->call(UserSeeder::class);
+        } catch (\Throwable $th) {
+            //throw $th;
+        }
+    }
+}
